814a17425a5177c0f5fbe4947246785c0a31019a
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-14 20:09:41 -0300

    chore: add createApp method

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,16 +5,22 @@ import { connectDB, handleShutdown } from "../config/database";
 
 dotenv.config();
 
-const app = express();
+export const createApp = () => {
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-app.use("/shipper", shipperRoutes);
+  app.use("/shipper", shipperRoutes);
 
-connectDB(process.env.MONGODB_URI || "").then(() => {
-  app.listen(process.env.PORT || 3000, () =>
-    console.log(`Server running on port ${process.env.PORT || 3000}`)
-  );
-  handleShutdown("SIGINT");
-  handleShutdown("SIGTERM");
-});
+  connectDB().then(() => {
+    app.listen(process.env.PORT || 3000, () =>
+      console.log(`Server running on port ${process.env.PORT || 3000}`)
+    );
+    handleShutdown("SIGINT");
+    handleShutdown("SIGTERM");
+  });
+
+  return app;
+};
+
+export const app = createApp();
