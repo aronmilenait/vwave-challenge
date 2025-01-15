c2f93edddc9d602315c3e0b79afc882330e25440
Milena Sol Aron <aronmilena.it@gmail.com>
2025-01-15 08:51:29 -0300

    feat: create getLabelsFromDB method

--- backend/src/modules/shipper/repository/labelRepository.ts
+++ backend/src/modules/shipper/repository/labelRepository.ts
@@ -1,7 +1,7 @@
 import { model } from "mongoose";
 import { labelSchema } from "./schemas/labelSchema";
 import { ILabel } from "./interfaces/labelInterface";
-import { createLabelError } from "./errors";
+import { createLabelError, getLabelsFromDBError } from "./errors";
 
 const Label = model<ILabel>("Label", labelSchema);
 
@@ -11,5 +11,13 @@ export const createLabelInDB = async (label: ILabel) => {
     return await newLabel.save();
   } catch {
     throw new Error(createLabelError);
+  }
+};
+
+export const getLabelsFromDB = async () => {
+  try {
+    return await Label.find();
+  } catch {
+    throw new Error(getLabelsFromDBError);
   }
 };
